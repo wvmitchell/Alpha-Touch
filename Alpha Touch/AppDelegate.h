5eed270babb0e9726d6031d95f2a1e9719e74cdd
William Mitchell <wvmitchell@Williams-MacBook-Air.local>
2014-09-22 11:23:55 -0600

    Initial Commit

---
+++ Alpha Touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alpha Touch
+//
+//  Created by William Mitchell on 9/22/14.
+//  Copyright (c) 2014 William Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alpha Touch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alpha Touch
+//
+//  Created by William Mitchell on 9/22/14.
+//  Copyright (c) 2014 William Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
